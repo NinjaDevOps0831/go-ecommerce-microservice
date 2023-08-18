4d051053d1d0cffd8be502f83828c53927f60d74
Daniel Smith <dalejandrole@gmail.com>
2023-08-18 12:24:30 +0530

    correcting jwt and twilio

--- go-ecommerce-auth-svc/pkg/config/config.go
+++ go-ecommerce-auth-svc/pkg/config/config.go
@@ -1,3 +1,4 @@
+/*
 package config
 
 import (
@@ -204,3 +205,57 @@ func GetJWTConfig() string {
 }
 
 */
+
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port string `mapstructure:"PORT"`
+	//DBUrl string `mapstructure:"DB_URL"`
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBName     string `mapstructure:"DB_NAME"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+
+	JWT string `mapstructure:"JWT_CODE"`
+
+	AUTHTOKEN  string `mapstructure:"TWILIO_AUTH_TOKEN"`
+	ACCOUNTSID string `mapstructure:"TWILIO_ACCOUNT_SID"`
+	SERVICESID string `mapstructure:"TWILIO_SERVICE_SID"`
+}
+
+var envs = []string{"PORT", "DB_HOST", "DB_NAME", "DB_USER", "DB_PORT", "DB_PASSWORD",
+	"JWT_CODE",
+	"TWILIO_AUTH_TOKEN", "TWILIO_ACCOUNT_SID", "TWILIO_SERVICE_SID",
+}
+var config *Config
+
+func LoadConfig() (*Config, error) {
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
+	for _, env := range envs {
+		if err := viper.BindEnv(env); err != nil {
+			return nil, err
+		}
+	}
+
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func GetConfig() Config {
+	return *config
+}
+
+func GetJWTConfig() string {
+	return config.JWT
+}
