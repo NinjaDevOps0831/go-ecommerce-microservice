fec0ea5914ae86a135c0c45932fb296f577b075a
Daniel Smith <dalejandrole@gmail.com>
2023-08-21 17:22:20 +0530

    doing product svc- usecase completed

---
+++ go-ecommerce-product-svc/pkg/usecase/interface/product.go
@@ -0,0 +1,28 @@
+package interfaces
+
+import (
+	"context"
+
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/domain"
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/model/common"
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/model/request"
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/model/response"
+)
+
+type ProductUseCase interface {
+	CreateCategory(ctx context.Context, newCategory string) (domain.ProductCategory, error)
+	//ListAllCategories(ctx context.Context) ([]domain.ProductCategory, error)
+	//FindCategoryByID(ctx context.Context, categoryID int) (domain.ProductCategory, error)
+	//UpdateCategory(ctx context.Context, updateCatInfo domain.ProductCategory) (domain.ProductCategory, error)
+	//DeleteCategory(ctx context.Context, categoryID int) (domain.ProductCategory, error)
+
+	CreateProduct(ctx context.Context, newProduct domain.Product) (domain.Product, error)
+	ListAllProducts(ctx context.Context, viewProductsQueryParam common.QueryParams) ([]response.ViewProduct, error)
+	//FindProductByID(ctx context.Context, productID int) (domain.Product, error)
+	//UpdateProduct(ctx context.Context, updateProductInfo domain.Product) (domain.Product, error)
+	//DeleteProduct(ctx context.Context, productID int) error
+
+	CreateBrand(ctx context.Context, newBrandDetails domain.ProductBrand) (domain.ProductBrand, error)
+
+	AddProductDetails(ctx context.Context, NewProductDetails request.NewProductDetails) (domain.ProductDetails, error)
+}
---
+++ go-ecommerce-product-svc/pkg/usecase/product.go
@@ -0,0 +1,69 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/domain"
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/model/common"
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/model/request"
+	"github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/model/response"
+	interfaces "github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/repository/interface"
+	services "github.com/ajujacob88/go-ecommerce-microservice-clean-arch/go-ecommerce-product-svc/pkg/usecase/interface"
+)
+
+type productUseCase struct {
+	productRepo interfaces.ProductRepository
+}
+
+func NewUserUseCase(repo interfaces.ProductRepository) services.ProductUseCase {
+	return &productUseCase{
+		productRepo: repo,
+	}
+
+}
+
+func (c *productUseCase) CreateCategory(ctx context.Context, newCategory string) (domain.ProductCategory, error) {
+	createdCategory, err := c.productRepo.CreateCategory(ctx, newCategory)
+	if err != nil {
+		return domain.ProductCategory{}, err
+	}
+	return createdCategory, nil
+}
+
+// -----------Brand Management -----------
+func (c *productUseCase) CreateBrand(ctx context.Context, newBrandDetails domain.ProductBrand) (domain.ProductBrand, error) {
+	createdBrand, err := c.productRepo.CreateBrand(ctx, newBrandDetails)
+	if err != nil {
+		return domain.ProductBrand{}, err
+	}
+	return createdBrand, nil
+}
+
+//----------Product Management
+
+func (c *productUseCase) CreateProduct(ctx context.Context, newProduct domain.Product) (domain.Product, error) {
+	createdProduct, err := c.productRepo.CreateProduct(ctx, newProduct)
+	if err != nil {
+		return domain.Product{}, err
+	}
+	return createdProduct, nil
+}
+
+func (c *productUseCase) ListAllProducts(ctx context.Context, viewProductsQueryParam common.QueryParams) ([]response.ViewProduct, error) {
+	allProducts, err := c.productRepo.ListAllProducts(ctx, viewProductsQueryParam)
+	if err != nil {
+		return nil, err
+	}
+	return allProducts, nil
+}
+
+//------ Product Details manage
+
+func (c *productUseCase) AddProductDetails(ctx context.Context, NewProductDetails request.NewProductDetails) (domain.ProductDetails, error) {
+	addedProdDetails, err := c.productRepo.AddProductDetails(ctx, NewProductDetails)
+	if err != nil {
+		return domain.ProductDetails{}, err
+	}
+	return addedProdDetails, nil
+
+}
